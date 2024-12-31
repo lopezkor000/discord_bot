cb9030ad8e90c0c7058624f756a8b6fd9d282970
Kory <74568018+lopezkor000@users.noreply.github.com>
2024-12-31 17:46:54 -0600

    add task to user

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-import discord, os
+import os, requests
 from discord import Message, Client, Intents
 
 class TaskManager:
   def __init__(self):
-    pass
+    self.BASEURL = "https://habitica.com/api/v3/"
+    self.token = os.environ.get('api_token')
+    self.user = os.environ.get('user_id')
+    self.headers = {"x-api-user":self.user, "x-api-key":self.token, "x-client":self.user+"-DiscordBot"}
+
+  def add_task(self, text:str, task_type:str, **kwargs):
+    params = {
+      "text": text,
+      "type": task_type
+    }
+    requests.post(url=self.BASEURL + 'tasks/user', headers=self.headers, data=params)
 
 class TaskBot(Client):
+  def __init__(self, *, intents, **options):
+    super().__init__(intents=intents, **options)
+    self.TaskManager = TaskManager()
+
   async def on_ready(self):
     print(f'Logged on as {self.user}')
 
@@ -31,6 +45,12 @@ class TaskBot(Client):
         await message.channel.delete_messages(messages)
       except Exception as e:
         print('Error:', e)
+
+    if message.content.startswith('$add_task'):
+      command = message.content.split(' ')
+      task_type = command[1]
+      task_name = " ".join(command[2:])
+      self.TaskManager.add_task(task_name, task_type)
 
 intents = Intents.default()
 intents.message_content = True
